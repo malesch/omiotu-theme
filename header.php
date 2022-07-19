32efd1050c82d12037625b49dfbcd99b398e881e
Marcus Spiegel <malex@calclab.com>
2022-07-19 09:33:57 +0200

    Update favicon

--- header.php
+++ header.php
@@ -6,7 +6,11 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
     <link href="//www.google-analytics.com" rel="dns-prefetch">
-    <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
+    <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/img/icons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon-16x16.png">
+    <link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/img/icons/site.webmanifest">
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,700,300&amp;subset=latin-ext' rel='stylesheet' type='text/css'>
 
     <?php wp_head(); ?>
@@ -48,8 +52,3 @@
         </div> <!-- /.row -->
       </div> <!-- /.container -->
     </nav>
-
-
-
-
-
